0e24840fd64afe81d1d67b60f96bc23d5a3fa68e
sonu simariya <lalitsimariya14@gmail.com>
2023-04-20 01:37:30 +0530

    ok

---
+++ foreign_key.sql
@@ -0,0 +1,16 @@
+use ls;
+drop table subjects;
+create table subjects(subject_id int auto_increment,primary key(subject_id),subject_name varchar(30));
+insert into subjects(subject_name) values('physics'),('maths'),('chemistry'),('maths');
+
+select * from subjects;
+
+
+drop table employee;
+create table employee(emp_id int not null, primary key(emp_id), f_name varchar(30),l_name varchar(30),id int,
+foreign key (id) references subjects(subject_id));
+
+insert into employee(emp_id,f_name,l_name,id) values(101,'akash','subodh',2),
+(104,'jcnjrr','jdjd',3),(103,'bhdhcba','jnvfirj',1);
+select * from employee;
+
