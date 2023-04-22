dd1f87d3c7245114514430719ee765cff8bbaffd
sonu simariya <lalitsimariya14@gmail.com>
2023-04-22 19:30:44 +0530

    ok

--- natural_join.sql
+++ natural_join.sql
@@ -9,3 +9,6 @@ where subjects.subject_id = employee.id;
 update employee set f_name='sonu',l_name='simariya' where emp_id=101;
 update employee set f_name='lalit',l_name='jsai' where emp_id=102;
 update employee set id=1 where f_name='sonu';
+-- equi join is similar to natural join but differnece is: natural join only join the simailar key value 
+-- but equi join joins any column with another table column 
+select * from employee,subjects where subjects.subject_id=employee.id;
